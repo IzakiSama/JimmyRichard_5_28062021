fdbf7a46946b169815de2d13659657891bf032a0
Jimmy Richard <jiji787sang@hotmail.fr>
2021-07-07 12:36:56 +0200

    vers.0.0.1

---
+++ index.js
@@ -0,0 +1,61 @@
+
+// On appelle l'API avec fetch
+fetch("http://localhost:3000/api/teddies")
+  .then(function(res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(function(teddies) {
+    // console.log(teddies);
+    createTeddies(teddies)
+  })
+  .catch(function(err) {
+    // Une erreur est survenue
+  });
+
+// On crée une fonction qui parcourt les valeurs de l'API
+function createTeddies (teddies) {
+  for (const teddy of teddies) {
+    createTeddy(teddy);
+  }
+};
+
+// On crée une fonction qui crée des éléments dans le DOM
+function createTeddy(teddy) {
+  const divProducts = document.getElementById('products');
+  
+  const imgTeddy = document.createElement('img');
+  imgTeddy.src = teddy.imageUrl;
+  imgTeddy.alt = teddy.name;
+  divProducts.appendChild(imgTeddy);
+
+  const paraName = document.createElement('p');
+  paraName.textContent = teddy.name;
+  divProducts.appendChild(paraName);
+
+  const paraPrice = document.createElement('p');
+  paraPrice.textContent = teddy.price;
+  divProducts.appendChild(paraPrice);
+
+  const linkDetail = document.createElement('a');
+  linkDetail.href = "produit.html?id=" + teddy._id;
+  linkDetail.textContent = "+ de détail";
+  divProducts.appendChild(linkDetail);
+};
+
+
+
+
+
+
+// var section = document.querySelector('section');
+
+// var paragraphe = document.createElement('p');
+// paragraphe.textContent = 'We hope you enjoyed the ride.';
+// section.appendChild(paragraphe);
+
+// var text = document.createTextNode(' — the premier source for web development knowledge.');
+
+// var linkPara = document.querySelector('p');
+// linkPara.appendChild(text);
---
+++ produit.js
@@ -0,0 +1,40 @@
+
+// On appelle l'API avec fetch
+fetch("http://localhost:3000/api/teddies")
+  .then(function(res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(function(teddies) {
+    console.log(teddies);
+    createTeddies(teddies)
+  })
+  .catch(function(err) {
+    // Une erreur est survenue
+  });
+
+  // On crée une fonction qui parcourt les valeurs de l'API
+function createTeddies (teddies) {
+    for (const teddy of teddies) {
+      createTeddy(teddy);
+    }
+  };
+  
+  // On crée une fonction qui crée des éléments dans le DOM
+  function createTeddy(teddy) {
+    const divProducts = document.getElementById('products');
+    
+    const imgTeddy = document.createElement('img');
+    imgTeddy.src = teddy.imageUrl;
+    imgTeddy.alt = teddy.name;
+    divProducts.appendChild(imgTeddy);
+  
+    const paraName = document.createElement('p');
+    paraName.textContent = teddy.name;
+    divProducts.appendChild(paraName);
+  
+    const paraPrice = document.createElement('p');
+    paraPrice.textContent = teddy.price;
+    divProducts.appendChild(paraPrice);
+  }
